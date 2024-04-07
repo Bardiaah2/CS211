cb509e22f5aea1e0f52577a2b746cc3943238e91
bardia <bardiaah@uoregon.edu>
2024-04-07 14:57:16 -0700

    Class Update

--- 211/class_1.py
+++ 211/class_1.py
@@ -21,6 +21,52 @@ class Point:
 
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
+    
+#-----------------------------------------------------------#
+
+def all_same(l: list) -> bool:
+    i, j = 0 ,1
+    if len(l) > 1:
+        while j < len(l):
+            if l[i] != l[j]:
+                return False
+            i += 1
+            j += 1
+    
+    return True
+
+def dedup(l: list) -> list:
+    i, j = 0, 1
+    l_copy = l[:]
+
+    if len(l_copy) > 1:
+
+        while j < len(l_copy):
+
+            if l_copy[i] == l_copy[j]:
+                del l_copy[j]
+
+            else:
+                i += 1
+                j += 1
+    
+    return l_copy
+
+def max_run(l: list) -> int:
+    i, j = 0, 1
+    curr_max = 0
+    if len(l) >= 1:
+        curr_max += 1
+        while j < len(l):
+            
+            if l[i] == l[j]:
+                j += 1
+            else:
+                curr_max = max(curr_max, j-i)
+                i = j + 1
+                j += 2
+    
+    return curr_max
 
 if __name__ == '__main__':
     p = Point(3, 4)
@@ -31,3 +77,17 @@ if __name__ == '__main__':
     print(m)
 
     assert m.x == 8 and m.y == 10
+
+    #-------------------------------------------------------#
+    print("all_same:")
+    print(all_same([1, 1 ,1, 1]),
+          all_same([]), 
+          all_same([1, 3, 1]))
+    
+    print('dedup:')
+    print(dedup([1, 1, 2, 1, 1]))
+    print(dedup([1]))
+
+    print('max_run:')
+    print(max_run([1, 1, 2, 2, 3, 4, 4, 4, 2, 4, 4])) # 3
+    print(max_run([])) # 0
